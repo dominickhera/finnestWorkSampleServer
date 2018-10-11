76897dd6ee390d44057bb9daf6b6352249b430c8
Dominick Hera <dominickhera@me.com>
2018-10-10 21:06:07 -0400

    pls work

--- index.js
+++ index.js
@@ -6,7 +6,170 @@ var ParseServer = require('parse-server').ParseServer;
 var path = require('path');
 
 var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
-
+var transactionArray = [
+  {
+      description: "POS Walmart",
+      amount: '200',
+      date: '2018-02-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS chipotle',
+      amount: '1000',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS Kmart',
+      amount: '3000',
+      date: '2018-11-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS Macdonalds',
+      amount: '400',
+      date: '2018-09-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS alcohol stuff',
+      amount: '2500',
+      date: '2018-08-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS google',
+      amount: '1200',
+      date: '2018-09-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS apple',
+      amount: '9920',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS über',
+      amount: '20',
+      date: '2018-02-10T14:21:19.465Z'
+    },
+    
+    {
+      description: 'Greatest Finnest',
+      amount: '20226',
+      date: '2018-03-10T14:21:19.465Z'
+    },
+    {
+      description: 'sop dog',
+      amount: '2660',
+      date: '2018-04-10T14:21:19.465Z'
+    },
+    {
+      description: 'stoff',
+      amount: '2450',
+      date: '2018-05-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS Metro',
+      amount: '2054',
+      date: '2018-07-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS Walgreens',
+      amount: '1000',
+      date: '2018-06-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS samsung',
+      amount: '7600',
+      date: '2018-09-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS twitter',
+      amount: '2029',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS Burger King',
+      amount: '1997',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS Walmart',
+      amount: '2000',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS chipotle',
+      amount: '1000',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS Kmart',
+      amount: '3000',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS Macdonalds',
+      amount: '400',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS alcohol stuff',
+      amount: '2500',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS google',
+      amount: '1200',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS apple',
+      amount: '9920',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS über',
+      amount: '20',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    
+    {
+      description: 'Greatest Finnest',
+      amount: '20226',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'sop dog',
+      amount: '2660',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'stoff',
+      amount: '2450',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS Metro',
+      amount: '2054',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS Walgreens',
+      amount: '1000',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS samsung',
+      amount: '7600',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS twitter',
+      amount: '2029',
+      date: '2018-10-10T14:21:19.465Z'
+    },
+    {
+      description: 'POS Burger King',
+      amount: '1997',
+      date: '2018-10-10T14:21:19.465Z'
+    }
+];
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
@@ -43,6 +206,15 @@ app.get('/', function(req, res) {
 // Remove this before launching your app
 app.get('/test', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/test.html'));
+});
+
+app.get('/initTransactionList', function(req, res) {
+  var initTransactionList = [];
+  var i;
+  for(i = 0; i < 10; i++) {
+    initTransactionList.push(transactionArray[i]);
+  }
+  res.send(initTransactionList);
 });
 
 var port = process.env.PORT || 1337;
